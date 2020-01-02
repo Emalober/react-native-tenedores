d2980b54432b98c40c8325ea36e37bf16aa0d140
Ezequiel Maloberti <emaloberti@technisys.com>
2020-01-01 23:22:59 -0300

    Escribiendo la página de usuarios no logeados UserGuest

--- app/screens/account/UserGuest.tsx
+++ app/screens/account/UserGuest.tsx
@@ -1,10 +1,61 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { StyleSheet, View, Text, ScrollView, Image } from "react-native";
+import { Button } from "react-native-elements";
 
 export default function UserGuest() {
     return (
-        <View>
-            <Text>Usuario invitado </Text>
-        </View>
+        <ScrollView style={styles.viewBody} centerContent={true}> 
+        <Image 
+            source={require("../../../assets/img/user-guest.jpg")}
+            style={styles.image}
+            resizeMode="contain"/>
+            <Text style={styles.text}>
+                Consulta tu perfil de 5 Tenedores
+            </Text>
+            <Text style={styles.description}>
+                ¿Como describirias tu mejor restauranes? Busca y visualiza los mejores lugares
+                para comer. Vota los que mas te han gustado y comenta tu experiencia.
+            </Text>
+            <View style={styles.viewBtn}>
+                <Button 
+                    buttonStyle={styles.btn}
+                    containerStyle={styles.btnContainer}
+                    title="Ver tu perfil"
+                    onPress={ () => console.log("On press...") }/>
+            </View>
+        </ScrollView>
     );
 }
+
+const styles = StyleSheet.create({
+    viewBody: {
+        marginLeft: 30,
+        marginRight: 30,
+
+    },
+    image: {
+        height: 300,
+        width: "100%",
+        marginBottom: 40
+    },
+    text: {
+        fontWeight: 'bold',
+        fontSize: 19,
+        marginBottom: 10,
+        textAlign: "center"
+    },
+    description: {
+        marginBottom: 20,
+        textAlign: "center"
+    },
+    viewBtn: {
+        flex: 1,
+        alignItems: "center"
+    },
+    btn: {
+        backgroundColor: "#00a680"
+    },
+    btnContainer: {
+        width: "70%"
+    }
+})
