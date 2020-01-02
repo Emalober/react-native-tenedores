6855f2746bb83672463fc8f660a83f831cffe3e8
Ezequiel Maloberti <emaloberti@technisys.com>
2020-01-01 22:55:32 -0300

    Creando screen UserGuest y UserLogged

--- app/screens/account/MyAccount.tsx
+++ app/screens/account/MyAccount.tsx
@@ -2,6 +2,8 @@ import React, { useState, useEffect } from "react";
 import { auth } from "../../utils/firebase"
 import { View, Text } from "react-native";
 import Loading from "../../components/Loading"
+import UserGuest from "./UserGuest"
+import UserLogged from "./UserLogged"
 
 export default function MyAccount() {
 
@@ -19,18 +21,5 @@ export default function MyAccount() {
             <Loading isVisible={true} text="Cargando..." />
         );
     }
-
-    if(login) {
-        return (
-            <View>
-                <Text>Usuario logueado </Text>
-            </View>
-        );
-    }
-
-    return (
-        <View>
-            <Text>Usuario no logueado </Text>
-        </View>
-    );
+    return login ? <UserLogged/> : <UserGuest/>
 }
